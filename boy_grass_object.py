62d7af665eaa1a5f2d34ed46c63109249e078f90
emldemld <85053469+emldemld@users.noreply.github.com>
2021-10-20 19:53:55 +0900

    dir, dr 내부로

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -15,10 +15,10 @@ class character:
         self.j = -20
         self.frame = 0
         self.state = 5
-
+        self.dir = 0
+        self.dr = 1
     def update(self):
-        global dir
-        self.x += dir * 10
+        self.x += self.dir * 10
         self.frame = (self.frame+1) % 3
         if self.j > 0:
             self.y += self.j
@@ -29,59 +29,51 @@ class character:
         else:
             self.j = -20
     def draw(self):
-        global dr
-        if dr == 1 and dir != 0 and self.j == -20:
+        if self.dr == 1 and self.dir != 0 and self.j == -20:
             self.image.clip_draw(self.frame * 60 + 480, self.state * 64, 64, 60, self.x, self.y)
-        elif dr == -1 and dir != 0 and self.j == -20:
+        elif self.dr == -1 and self.dir != 0 and self.j == -20:
             self.image.clip_draw(self.frame * -60 + 300, self.state * 64, 64, 60, self.x, self.y)
-        elif dr == 1 and dir == 0 and self.j == -20:
+        elif self.dr == 1 and self.dir == 0 and self.j == -20:
             self.image.clip_draw(420, self.state * 64, 64, 60, self.x, self.y)
-        elif dr == -1 and dir == 0 and self.j == -20:
+        elif self.dr == -1 and self.dir == 0 and self.j == -20:
             self.image.clip_draw(360, self.state * 64, 64, 60, self.x, self.y)
-        elif dr == 1 and self.j != -20:
+        elif self.dr == 1 and self.j != -20:
             self.image.clip_draw(720, self.state * 64, 64, 60, self.x, self.y)
-        elif dr != 1 and self.j != -20:
+        elif self.dr != 1 and self.j != -20:
             self.image.clip_draw(60, self.state * 64, 64, 60, self.x, self.y)
 
 
-class enemy:
-    def __init__(self):
-        self.image = load_image('enemies.png')
-        self.x, self.y = 400, 80
-        self.j = -20
-        self.frame = 0
-        self.state = 5
+# class enemy:
+#     def __init__(self):
+#         self.image = load_image('enemies.png')
+#         self.x, self.y = 600, 80
+#         self.frame = 0
+#         self.dir = 1
+#
+#     def update(self):
+#         self.x += self.dir * 5
+#         self.frame = (self.frame + 1) % 2
+#
+#     def draw(self):
+#         if self.dr == 1 and self.dir != 0 and self.j == -20:
+#             self.image.clip_draw(self.frame * 60 + 480, self.state * 64, 64, 60, self.x, self.y)
+#         elif self.dr == -1 and self.dir != 0 and self.j == -20:
+#             self.image.clip_draw(self.frame * -60 + 300, self.state * 64, 64, 60, self.x, self.y)
 
-    def update(self):
-        global dir
-        self.x += dir * 10
-        self.frame = (self.frame + 1) % 3
-        if self.j > 0:
-            self.y += self.j
-            self.j -= 1
-        elif self.j <= 0 and self.j > -20:
-            self.j -= 1
-            self.y += self.j
-        else:
-            self.j = -20
-
-    def draw(self):
 
 def handle_events():
     global running
-    global dir
-    global dr
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_d:
-               dir += 1
-               dr = 1
+               character.dir += 1
+               character.dr = 1
             elif event.key == SDLK_a:
-                dr = -1
-                dir -= 1
+                character.dr = -1
+                character.dir -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
             elif event.key == SDLK_SPACE:
@@ -89,9 +81,9 @@ def handle_events():
 
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_d:
-                dir -= 1
+                character.dir -= 1
             elif event.key == SDLK_a:
-                dir += 1
+                character.dir += 1
             elif event.key == SDLK_SPACE:
                 character.j = -character.j
 
@@ -102,8 +94,6 @@ open_canvas(1000, 500)
 bg =bg()
 character = character()
 
-dr = 1
-dir = 0
 running = True
 
 # game main loop code
